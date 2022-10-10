b1f0e084f22bbf6da7617aa528f837dffc067ab8
e9995b35 <fh322@ic.ac.uk>
2022-10-10 15:21:22 +0100

    <my commit message>

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,9 @@
 import numpy as np
+import pandas as pd
 from scipy.ndimage import gaussian_filter
 from scipy import misc
 
-__all__ = ['rand_array', 'smooth_image','my_mat_solve']
+__all__ = ['rand_array', 'smooth_image','my_mat_solve','my_test']
 
 
 def rand_array(shape):
@@ -13,4 +14,7 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def my_test(s):
+    return pd.Series([1, 3, s, np.nan, 6, 8])
     
---
+++ scripts/my_test.py
@@ -0,0 +1,6 @@
+from envtest import my_test
+
+A = 6
+B = my_test(A)
+
+print(B)
